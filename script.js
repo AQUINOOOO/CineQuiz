e75052b95b7ffb5600ce8b500355a3aeb1ae342a
AQUINOOOO <159671138+AQUINOOOO@users.noreply.github.com>
2025-07-17 11:02:07 -0300

    finalizando HTML E CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        questions: "Qual e seu gênero de filme?",
+        answers: [
+              {id:1, text:"Romance" , correct:true},
+              {id:2, text:"Terror" , correct:true},
+                {id:3, text: "Animação" , correct:true} ,
+                  {id:4, text:"Ação" ,correct:true} ,
+        ]
+    }
+]
