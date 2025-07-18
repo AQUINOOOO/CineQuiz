f15252e2bcdf9feb18ae26a3bd6f6e7d97b3d71b
AQUINOOOO <159671138+AQUINOOOO@users.noreply.github.com>
2025-07-18 15:42:54 -0300

    colocando o texto na tela

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,4 +1,4 @@
- export const quizArrays = [
+ const questions = [
     {
         questions: " O que você mais gosta de sentir ao ver um filme?",
         answers: [
@@ -16,14 +16,41 @@
                 {id:3, text: "Uma casa abandonada no meio do nada" , correct:true} ,
                   {id:4, text:" Paris sob o pôr do sol" ,correct:true} ,
         ]
+    }, {
+        questions: ". Qual dessas frases combina mais com você?",
+        answers: [
+              {id:1, text:"Vamos resolver isso agora!" , correct:true},
+              {id:2, text:"Preciso de algo leve e divertido" , correct:true},
+                {id:3, text: "Se eu pular do sofá, é bom!" , correct:true} ,
+                  {id:4, text:"O amor sempre vence" ,correct:true} ,
+        ]
+    }, {
+        questions: "Seu personagem principal favorito seria",
+        answers: [
+              {id:1, text:"Um herói corajoso" , correct:true},
+              {id:2, text:" Um atrapalhado de coração bom" , correct:true},
+                {id:3, text: "Uma sobrevivente determinada" , correct:true} ,
+                  {id:4, text:"Um romântico incurável" ,correct:true} ,
+        ]
     }
-]
- let i = 0
- if(quizArrays[i] == 0){
-   console.log(quizArrays[0])
-   i += 1
- }
- else if(quizArrays[i] == 1){
-   console.log(quizArrays[1])
-   i += 1
- }
+]  
+let questionsElement = document.getElementById('question')
+let answersButtons = document.getElementById('answer-buttons')
+let nexButton = document.getElementById('next-btn')
+let currentQuestionIndex = 0
+let score = 0
+function startQuiz(){
+  currentQuestionIndex = 0
+  score = 0 
+  nexButton.innerHTML = 'Proximo'
+  showQuestion()
+}
+function showQuestion(){
+  let currentQuestion = questions[currentQuestionIndex]
+   questionsElement.innerHTML = currentQuestion.questions
+}
+  startQuiz()
+
+
+  
+
